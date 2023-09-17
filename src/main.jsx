f4d01e397869338fe424580da60c06ae90e3106e
Hershell Koger <89715064+kogerhj@users.noreply.github.com>
2023-09-17 08:07:45 -0400

    create stock data file as a projects placeholder; input basic projects markup and styling (needs lots of work); currently commenting out Projects component and its references to work on other components

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import About from "./Components/About.jsx";
 import Contacts from "./Components/Contacts.jsx";
 import Navbar from "./Components/Navbar.jsx";
-import Projects from "./Components/Projects.jsx";
+// import Projects from "./Components/Projects.jsx";
 import Skills from "./Components/Skills.jsx";
 import Testimonials from "./Components/Testimonials.jsx";
 import Home from "./Components/Home.jsx";
@@ -13,10 +13,10 @@ import Home from "./Components/Home.jsx";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Navbar/>
-      <About/>
       <Home />
+      <About/>
       <Contacts/>
-      <Projects />
+      {/*<Projects />*/}
       <Skills />
       <Testimonials />
   </React.StrictMode>,
