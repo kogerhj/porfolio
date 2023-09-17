da2a72e38821239b7cf6a29066bbbd00e8abdcd6
Hershell Koger <89715064+kogerhj@users.noreply.github.com>
2023-09-17 08:09:37 -0400

    Renamed Contacts.jsx to Contact.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import About from "./Components/About.jsx";
-import Contacts from "./Components/Contacts.jsx";
+import Contact from "./Components/Contact.jsx";
 import Navbar from "./Components/Navbar.jsx";
 // import Projects from "./Components/Projects.jsx";
 import Skills from "./Components/Skills.jsx";
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Navbar/>
       <Home />
       <About/>
-      <Contacts/>
+      <Contact/>
       {/*<Projects />*/}
       <Skills />
       <Testimonials />
