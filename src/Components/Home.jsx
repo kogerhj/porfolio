dba951f2f2b9d77ceb9e823598fb438e991720f5
Hershell Koger <89715064+kogerhj@users.noreply.github.com>
2023-10-01 08:26:48 -0400

    Remove unused code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -25,12 +25,6 @@ return (
                     </a>
                 </div>
             </div>
-            {/*<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">*/}
-            {/*    <img*/}
-            {/*        className="object-cover object-center rounded"*/}
-            {/*        alt="IMAGE/SVG"*/}
-            {/*    />*/}
-            {/*</div>*/}
         </div>
     </section>
 )
